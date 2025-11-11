1fdead533d6c112e4992d9b7d2277194e95767f7
aylia03 <aylia.mayer@gmail.com>
2025-11-11 13:45:20 +0100

    first working solution

--- Cutter.py
+++ Cutter.py
@@ -4,9 +4,30 @@ import vtk
 
 def visualize(data_source, renderer):
     # Create a plane inside dataset
-
+    center = data_source.GetCenter()
+    cutting_plane = vtk.vtkPlane()
+    cutting_plane.SetOrigin(center[0],center[1],center[2])
+    cutting_plane.SetNormal(0,0,1)
     # Create cutter
+    cutter = vtk.vtkCutter()
+    cutter.SetInputData(data_source)
+    cutter.SetCutFunction(cutting_plane)
+    cutter.Update()
 
     # Create lookuptable
+    lut = vtk.vtkLookupTable()
+    lut.SetNumberOfColors(256)
+    lut.Build()
 
     # Add mapper and actor
+    cutter_mapper = vtk.vtkPolyDataMapper()
+    cutter_mapper.SetInputConnection(cutter.GetOutputPort())
+    cutter_mapper.SetLookupTable(lut)
+
+    output = cutter.GetOutput()
+    scalar_range = output.GetScalarRange()
+    cutter_mapper.SetScalarRange(scalar_range)
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(cutter_mapper)
+    renderer.AddActor(actor)
