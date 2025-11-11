c37719ed4e4c8be6a739d14dce559b6a08dcb8e6
aylia03 <aylia.mayer@gmail.com>
2025-11-11 14:25:37 +0100

    added comments

--- Cutter.py
+++ Cutter.py
@@ -4,31 +4,33 @@ import vtk
 
 def visualize(data_source, renderer):
     # Create a plane inside dataset
-    center = data_source.GetCenter()
-    cutting_plane = vtk.vtkPlane()
-    cutting_plane.SetOrigin(center[0],center[1],center[2]-0.5)
-    cutting_plane.SetNormal(-3,0,7)
+    center = data_source.GetCenter() # get center coordinates of the dataset
+    cutting_plane = vtk.vtkPlane() # create a plane
+    cutting_plane.SetOrigin(center[0],center[1],center[2]-0.5) # set position of plane
+    cutting_plane.SetNormal(-3,0,7) # defines the orientation of the plane
 
     # Create cutter
-    cutter = vtk.vtkCutter()
-    cutter.SetInputData(data_source)
-    cutter.SetCutFunction(cutting_plane)
-    cutter.Update()
+    cutter = vtk.vtkCutter() # create cutter
+    cutter.SetInputData(data_source) # connects the cutter with the dataset
+    cutter.SetCutFunction(cutting_plane) # set plane as cutting function
+    cutter.Update() # update cutter to process the data
 
     # Create lookuptable
-    lut = vtk.vtkLookupTable()
-    lut.SetNumberOfColors(256)
+    lut = vtk.vtkLookupTable() # create lookup table
+    lut.SetNumberOfColors(256) # settting of colors all hex combinations
     lut.Build()
 
     # Add mapper and actor
     cutter_mapper = vtk.vtkPolyDataMapper()
-    cutter_mapper.SetInputConnection(cutter.GetOutputPort())
-    cutter_mapper.SetLookupTable(lut)
+    cutter_mapper.SetInputConnection(cutter.GetOutputPort()) # connect cutter to mapper
+    cutter_mapper.SetLookupTable(lut) # connecet the table to mapper
 
+    # get scalar range from data for color mapping
     output = cutter.GetOutput()
     scalar_range = output.GetScalarRange()
     cutter_mapper.SetScalarRange(scalar_range)
 
+    # create actor and add to renderer
     actor = vtk.vtkActor()
     actor.SetMapper(cutter_mapper)
     renderer.AddActor(actor)
